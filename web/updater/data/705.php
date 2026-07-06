227e1497c1fbcf938d1eb77db7c850382621895c
Maximiliano Jabase <maxijabase@gmail.com>
2026-07-06 19:31:12 -0300

    Remove updater directory and upgrade.php entrypoint

--- web/updater/data/705.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->dbs->query("ALTER TABLE `:prefix_comms` MODIFY COLUMN `adminIp` VARCHAR(128) NOT NULL default ''");
-$this->dbs->execute();
-
-$this->dbs->query("ALTER TABLE `:prefix_bans` MODIFY COLUMN `adminIp` VARCHAR(128) NOT NULL default ''");
-$this->dbs->execute();
-
-return true;
--- web/updater/data/801.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$this->dbs->query("ALTER TABLE `:prefix_admins` ADD COLUMN `attempts` INT DEFAULT 0");
-$this->dbs->execute();
-
-$this->dbs->query("ALTER TABLE `:prefix_admins` ADD COLUMN `lockout_until` DATETIME DEFAULT NULL");
-$this->dbs->execute();
-
-return true;
